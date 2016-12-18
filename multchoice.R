e576a545a88fdf931228fe9f31def957539af85b
MikeGruz <mikewgruz@gmail.com>
2016-12-18 15:19:33 -0600

    added multi-choice function:   multchoice.R

---
+++ multchoice.R
@@ -0,0 +1,15 @@
+multTest = function(){
+  probText = "What is the median of the following set?"
+  probMath = round(rnorm(5, sample(0:5, 1), 1))
+  probMathDisp = paste(probMath, collapse=" ")
+  probSolution = median(probMath)
+  probChoices = c(probSolution, unique(probMath[!(probMath %in% probSolution)]))
+  return(list(
+    "text" = probText,
+    "problem" = probMath,
+    "problemDisp" = probMathDisp,
+    "solution" = probSolution,
+    "input" = "multi",
+    "choices" = probChoices
+    ))
+}
