4edec6bd2e332c2c784dfe70280ddec214cb51d4
MikeGruz <mikewgruz@gmail.com>
2017-09-20 15:37:47 -0500

    added assignment logic to db write

--- db/write_db.R
+++ db/write_db.R
@@ -1,9 +1,10 @@
 # function to write test results to file
 
-writeTest <- function(db, user_id, correct, method, answer, solution) {
+writeTest <- function(db, user_id, assign, correct, method, answer, solution) {
   outTable <- data.frame(
     user_id = user_id,
     date = Sys.time(),
+    assign = assign,
     correct = correct,
     method = method,
     answer = answer,
