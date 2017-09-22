3922cb5f01288c85407e04b96ffd28f8ce9832d0
MikeGruz <mikewgruz@gmail.com>
2017-09-21 22:34:07 -0500

    added feedback to mean computation trials

--- methods/meantest.R
+++ methods/meantest.R
@@ -1,14 +1,37 @@
 meantest = function(){
-  probText = "Find the mean of the following set of numbers"
-  probMath = round(rnorm(5, sample(0:5, 1), 1))
+  
+  # problem text
+  probText = "Find the mean of the following set of numbers (round to two decimal places)."
+  
+  # generate the set of numbers
+  probMath = round(rnorm(sample(3:7, size = 1), sample(0:5, 1), 1))
+  
+  # display the set
   probMathDisp = paste(probMath, collapse=" ")
-  probSolution = mean(probMath)
+  
+  # the solution
+  probSolution = round(mean(probMath), digits = 2)
+  
+  # feedback
+  feedback <- paste(
+    "The correct answer is <i>",
+    probSolution,
+    "</i>. ",
+    "We calculate the mean by first adding up all of the values in the set (",
+    sum(probMath),
+    ") and dividing by the total number of observations in the set (",
+    length(probMath),
+    ").",
+    sep = ""
+  )
+  
   return(list(
     "method" = "compute mean",
     "text" = probText,
     "problem" = probMath,
     "problemDisp" = probMathDisp,
     "solution" = probSolution,
-    "input" = "numeric"
+    "input" = "numeric",
+    "feedback" = feedback
     ))
 }
